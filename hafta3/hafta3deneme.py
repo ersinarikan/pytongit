37e55a3c75634d050b0a91de9ca8a7c204ddebc6
ersinarikan <147282199+ersinarikan@users.noreply.github.com>
2023-10-28 04:19:36 +0300

    ek

---
+++ hafta3/hafta3deneme.py
@@ -0,0 +1,29 @@
+#faiz hesaplama
+faturaTipi=(input("Fatura Tipini giriniz Gıda için (G), Elektronik için (E) "))
+faturaTutari=0
+fatura=0
+if(faturaTipi=='E'):
+    faturaTutari = int(input("Fatura Tutarını giriniz : "))
+    fatura=(faturaTutari*118/100)
+    print("Fatura Tutarı= : ",fatura," TL ")
+elif(faturaTipi=='e'):
+    faturaTutari = int(input("Fatura Tutarını giriniz : "))
+    fatura=(faturaTutari*118/100)
+    print("Fatura Tutarı= : ", fatura, " TL ")
+elif(faturaTipi=='G'):
+    faturaTutari = int(input("Fatura Tutarını giriniz : "))
+    fatura=(faturaTutari*108/100)
+    print("Fatura Tutarı= : ",fatura," TL ")
+elif(faturaTipi=='g'):
+    faturaTutari = int(input("Fatura Tutarını giriniz : "))
+    fatura=(faturaTutari*108/100)
+    print("Fatura Tutarı= : ",fatura," TL ")
+else:
+    print("Yanlış veri grişi")
+
+
+
+
+
+
+
